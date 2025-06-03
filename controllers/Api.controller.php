bc09eda0f40ab356b4b0b7f62316a2252e2e0385
khnadev <putukhana38@gmail.com>
2025-06-03 23:08:37 +0800

    Backend : update model

--- controllers/Api.controller.php
+++ controllers/Api.controller.php
@@ -2,17 +2,17 @@
 include_once("models/Model.model.php");
 class ApiController extends Model
 {
-    private $init;
-    public function __construct()
-    {
-        parent::__construct();
-    }
-    public function getallproduct()
-    {
-        createPublicAPI($this->getAll("barang"));
-    }
-    public function barang($value)
-    {
-        createPublicAPI(value: $this->getById("barang", "kd_brg", $value));
-    }
+    // private $init;
+    // public function __construct()
+    // {
+    //     parent::__construct();
+    // }
+    // public function getallproduct()
+    // {
+    //     createPublicAPI($this->getAll("barang"));
+    // }
+    // public function barang($value)
+    // {
+    //     createPublicAPI(value: $this->getById("barang", "kd_brg", $value));
+    // }
 }
